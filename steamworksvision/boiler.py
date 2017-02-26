348ede5f841700105b6161f7161f056b7915828a
tylerscheuble <tylerscheuble@gmail.com>
2017-02-25 20:07:04 -0800

    Updated boiler vision

--- steamworksvision/boiler.py
+++ steamworksvision/boiler.py
@@ -1,9 +1,38 @@
 import cv2
 
+INTENSITY_THRESHOLD = 254
+CONTOUR_SIZE_THRESHOLD = 500
+BOILER_TILT_ERROR = 12
+
+def contour_filter(contour):
+    return cv2.contourArea(contour) > CONTOUR_SIZE_THRESHOLD
+
+def locate_boiler(contours):
+    count = len(contours)
+    for i in range(0, count - 1):
+        fst_cnt = contours[i]
+        fst_leftmost_x = tuple(fst_cnt[fst_cnt[:,:,0].argmin()][0])[0]
+        fst_rightmost_x = tuple(fst_cnt[fst_cnt[:,:,0].argmax()][0])[0]
+        for j in range(i, count):
+            snd_cnt = contours[i]
+            snd_leftmost_x = tuple(snd_cnt[snd_cnt[:,:,0].argmin()][0])[0]
+            left_error = abs(fst_leftmost_x - snd_leftmost_x)
+            if left_error > BOILER_TILT_ERROR: break
+
+            snd_rightmost_x = tuple(snd_cnt[snd_cnt[:,:,0].argmax()][0])[0]
+            right_error = abs(fst_rightmost_x - snd_rightmost_x)
+            if right_error < BOILER_TILT_ERROR
+
+
+
 def run(q):
     while True:
         ir_img, depth_img = q.get()
 
+        ret, ir_img = cv2.threshold(ir_img, INTENSITY_THRESHOLD, 255, cv2.THRESH_BINARY)
+        ir_img, contours, heirarchy = cv2.findContours(ir_img, cv2.RETR_LIST)
+        contours = filter(contour_filter, contours)
         cv2.imshow('', ir_img)
-        cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            pass
         q.task_done()
--- steamworksvision/core.py
+++ steamworksvision/core.py
@@ -1,21 +1,41 @@
 import pyrealsense as pyrs
 from threading import Thread
-from queue import Queue
+from Queue import Queue, LifoQueue, Full as FullException
 
 from .boiler import run as run_boiler
+from .gear import run as run_gear
+from .network import run as run_network
 
 pyrs.start()
 rs = pyrs.Device()
 
-boiler_q = Queue(maxsize = 6)
-gear_q = Queue(maxsize = 6)
-pyrs.start()
+data_q = Queue()
 
+boiler_q = LifoQueue(maxsize = 6)
+gear_q = LifoQueue(maxsize = 6)
 
-boiler_thread = Thread(target=run_boiler, args = (rs,))
+boiler_thread = Thread(target=run_boiler, args = (boiler_q, data_q))
 boiler_thread.daemon = True
 boiler_thread.start()
 
+gear_thread = Thread(target=run_gear, args = (gear_q, data_q))
+gear_thread.daemon = True
+gear_thread.start()
+
+network_thread = Thread(target = run_network, args = (data_q,))
+
 ## main loop
 while True:
-    rs.wait_for_frame() # TODO: do stuff with frames
+    rs.wait_for_frame()
+    color_img = rs.colour
+    ir_img = rs.infrared2
+    depth_img = rs.depth
+
+    try:
+        boiler_q.put_nowait((ir_img, depth_img))
+    except FullException:
+        pass
+    try:
+        gear_q.put_nowait((ir_img, depth_img))
+    except FullException:
+        pass
